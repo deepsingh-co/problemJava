2da9de4c8952cb1cdb1c056c64c21c0e196aab08
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-08-15 21:04:30 +0530

    feat: implement Sudoku solver with backtracking algorithm

---
+++ s.java
@@ -0,0 +1,98 @@
+public class sudok {
+
+    public static void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private static boolean solve(char[][] board) {
+
+        // Find an empty cell
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+
+                if (board[row][col] == '.') {
+
+                    // Try digits 1 to 9
+                    for (char num = '1'; num <= '9'; num++) {
+
+                        if (isValid(board, row, col, num)) {
+
+                            // Place the number
+                            board[row][col] = num;
+
+                            // Recursively solve the rest
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            // Backtrack
+                            board[row][col] = '.';
+                        }
+                    }
+
+                    // No number works for this cell
+                    return false;
+                }
+            }
+        }
+
+        // No empty cells left
+        return true;
+    }
+
+    private static boolean isValid(char[][] board, int row, int col, char num) {
+
+        // Check row
+        for (int c = 0; c < 9; c++) {
+            if (board[row][c] == num) {
+                return false;
+            }
+        }
+
+        // Check column
+        for (int r = 0; r < 9; r++) {
+            if (board[r][col] == num) {
+                return false;
+            }
+        }
+
+        // Check 3x3 box
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int r = startRow; r < startRow + 3; r++) {
+            for (int c = startCol; c < startCol + 3; c++) {
+                if (board[r][c] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        char[][] board = {
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        solveSudoku(board);
+
+        // Print solved Sudoku
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
