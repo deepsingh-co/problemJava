5112f002a0c914cf4bfbccddeb837d9a4cfc9cd6
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-08-20 21:52:09 +0530

    feat: add distributedArray class with resultArray implementation

---
+++ distributedArray.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class distributedArray {
+
+    public static int[] resultArray(int[] nums) {
+        int n = nums.length;
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+
+        int size1 = 0;
+        int size2 = 0;
+
+        // First operation
+        arr1[size1++] = nums[0];
+
+        // Second operation
+        arr2[size2++] = nums[1];
+
+        // Remaining elements
+        for (int i = 2; i < n; i++) {
+            if (arr1[size1 - 1] > arr2[size2 - 1]) {
+                arr1[size1++] = nums[i];
+            } else {
+                arr2[size2++] = nums[i];
+            }
+        }
+
+        // Create result by concatenating arr1 and arr2
+        int[] result = new int[n];
+
+        for (int i = 0; i < size1; i++) {
+            result[i] = arr1[i];
+        }
+
+        for (int i = 0; i < size2; i++) {
+            result[size1 + i] = arr2[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 1, 3, 3};
+
+        int[] result = resultArray(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
