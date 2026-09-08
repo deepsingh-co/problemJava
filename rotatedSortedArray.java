5fac0970a9875286fd0771cd1f23b8831ebe5fbb
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-09-08 22:23:46 +0530

    feat: add binary search implementation for rotated sorted array

---
+++ rotatedSortedArray.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Target found
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Left half is sorted
+            if (nums[left] <= nums[mid]) {
+
+                // Target is in the left sorted half
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+
+            } 
+            // Right half is sorted
+            else {
+
+                // Target is in the right sorted half
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Example input:
+        // 4 5 6 7 0 1 2
+        // 0
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        System.out.println(search(nums, target));
+
+        sc.close();
+    }
+}
