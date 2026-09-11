008a2f070ad070cd9fb2030db98e067f6d5a3b59
deepsingh-co <deepsinghanirudh1@gmail.com>
2026-09-11 23:32:21 +0530

    feat: implement RotateMatrix class with matrix rotation functionality

---
+++ rotatematrix.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+public class RotateMatrix {
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Before rotation:");
+        printMatrix(matrix);
+
+        rotate(matrix);
+
+        System.out.println("After 90 degree clockwise rotation:");
+        printMatrix(matrix);
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
